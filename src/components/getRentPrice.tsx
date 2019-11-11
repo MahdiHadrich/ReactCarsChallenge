6ec0448669f4fe18ed9ef623649b6036986a7aeb
mahdihadrich <mahdo.ha@gmail.com>
2019-11-11 04:25:25 +0100

    Level 4

--- src/components/getRentPrice.tsx
+++ src/components/getRentPrice.tsx
@@ -1,6 +1,17 @@
 const getRentPrice = (distance: number, duration: number, car): number => {
   const { pricePerKm, pricePerDay } = car;
-  const price = (distance * pricePerKm) / 100 + (duration * pricePerDay) / 100;
+  let coefficient = 1;
+  if (duration > 10) {
+    coefficient = 0.5;
+  } else if (duration > 4) {
+    coefficient = 0.7;
+  } else if (duration > 1) {
+    coefficient = 0.9;
+  }
+
+  const price =
+    ((distance * pricePerKm) / 100 + (duration * pricePerDay) / 100) *
+    coefficient;
   const roundedPrice = Math.round(price * 100) / 100;
   return roundedPrice;
 };
